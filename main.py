6428e8ebe9e92b03a21c1b7b6fdfbf05538c30ce
Devin <devinwchen@gmail.com>
2015-11-07 08:16:41 -0800

    add MongoDB

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import pymongo
+from pymongo import MongoClient
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+client = MongoClient(MONGO_URL)
+
+#specify database
+db = client.heroku_v124pmq2
+collection = db.flow
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/", methods=['GET'])
+def index():
+    flow = collection.find()
+    return render_template('index.html', flow=flow)
+
+@app.route("/post", methods=['POST'])
+def index():
+    flow = {"name":request.form['name'], "message":request.form['message']}
+    flow_id = collection.insert(flow)
+    return redirect('/')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
