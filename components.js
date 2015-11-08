4fa99d87c13692d452a870ba29512bea9ad0f02a
Andy Chen <qq830214a@gmail.com>
2015-11-07 16:28:32 -0800

    intergrate the UI and logic part

--- components.js
+++ components.js
@@ -46,22 +46,36 @@ class App extends Component {
     }
     return (
       <div>
-        <button onClick={ this.handlePlayPause.bind(this) } disabled={!ready} >
-          {playing?'Pause':'Play'}
-        </button>
-        <button onClick={ this.handleNext.bind(this) } disabled={!ready} >
-          Next
-        </button>
-        <button onClick={ this.handleLike.bind(this) } disabled={!ready} >
-          Like
-        </button>
-        <button onClick={ this.handleDislike.bind(this) } disabled={!ready} >
-          Dislike
-        </button>
+		<div className="container text-center">
+  <div className="row">
+  <div className="col-md-3 col-lg-2"></div>
+  <div className="col-md-6 col-lg-8">
+  	<div className="playerCard">
+   	<img src="images/Flow Player2.png" className="logo"/>
+   	<h4>When you thumb up and thumb down, the player will be smarter!!</h4>
+
+
+   	 <img src="images/gestures.png" className="gestures"/>
+   </div>
+   <div className="playerCard">
+   	<div className="row">
+	  <div className="playerButton col-xs-3"><button id="unlike" className="glyphicon glyphicon-thumbs-down mainButton" onClick={ this.handleDislike.bind(this) } disabled={!ready}></button></div>
+	  <div className="playerButton col-xs-3"><button id="like" className="glyphicon glyphicon-thumbs-up mainButton" onClick={ this.handleLike.bind(this) } disabled={!ready}></button></div>
+	  <div className="playerButton col-xs-3"><button id="play" className="glyphicon  mainButton" onClick={ this.handlePlayPause.bind(this) } disabled={!ready}>{playing?'Pause':'Play'}</button></div>
+	  <div className="playerButton col-xs-3"><button id="next" className="glyphicon glyphicon-forward mainButton" onClick={ this.handleNext.bind(this) } disabled={!ready}></button></div>
+   	 </div>
+   </div>
+</div>
+   <div className="col-md-3 col-lg-2"></div>
+   </div>
+
+   <div className="teamInfo">Team of Flowplayer @ ATT IoT Hackathon (November 7)</div>
+
+</div>
 
         <div>
           <audio ref="player">
-            <source 
+            <source
               src="http://pd.npr.org/anon.npr-mp3/npr/me/2015/11/20151105_me_in_the_amazons_fire_season_you_either_burn_or_you_starve.mp3"
               type="audio/mpeg">
             </source>
@@ -74,4 +88,3 @@ class App extends Component {
 }
 
 export default connect((state)=>state)(App);
-
