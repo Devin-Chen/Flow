18cce8defff0466a94c8ba88ac36034f91632edc
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-11-07 17:02:24 -0800

    new stories

--- reducers.js
+++ reducers.js
@@ -15,20 +15,24 @@ let initialState = {
     },
     {
       title: 'test',
-      url: 'http://api.npr.org/m3u/8454829110-9ebe83.m3u?orgId=1&topicId=1025&d=420&p=3&story=453239276&t=progseg&e=454828028&seg=3&ft=nprml&f=',
+      url: 'http://pd.npr.org/anon.npr-mp3/npr/me/2015/11/20151105_me_as_taiwan_gears_up_for_elections_china_as_always_looms_large.mp3?orgId=1&topicId=1125&d=230&p=3&story=454576853&t=progseg&e=454828028&seg=15&ft=nprml&f=',
       opinion: Opinion.NEUTRAL,
     },
     {
       title: 'test',
-      url: 'http://api.npr.org/m3u/8454829128-54eb57.m3u?orgId=1&topicId=1125&d=230&p=3&story=454576853&t=progseg&e=454828028&seg=15&ft=nprml&f=',
+      url: 'http://pd.npr.org/anon.npr-mp3/npr/me/2015/11/20151105_me_powerful_gene_drive_can_quickly_change_an_entire_species.mp3?orgId=1&topicId=1128&d=300&p=3&story=451216596&t=progseg&e=454828028&seg=10&ft=nprml&f=',
 
       opinion: Opinion.NEUTRAL,
     },
     {
       title: 'test',
-      url: 'http://api.npr.org/m3u/8454829104-a6e371.m3u?orgId=1&topicId=1014&d=401&p=3&story=454702147&t=progseg&e=454828028&seg=1&ft=nprml&f=',
+      url: 'http://pd.npr.org/anon.npr-mp3/npr/me/2015/11/20151105_me_how_a_folding_electric_vehicle_went_from_car_of_the_future_to_obsolete.mp3?orgId=1&topicId=1019&aggIds=155914958&d=420&p=3&story=454693583&t=progseg&e=454828028&seg=11&ft=nprml&f=',
       opinion: Opinion.NEUTRAL,
     }
+
+
+
+
 
   ],
   playIndex: 0,
